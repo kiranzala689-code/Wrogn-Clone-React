030b3057ac65124e58e4dadaecf7632f0d9957e6
Kiran Zala <kiranzala689@gmail.com>
2026-08-18 14:26:06 +0530

    fix home and navbar warnings

--- src/Comp/Home.jsx
+++ src/Comp/Home.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SeasonTopPicks from "./SeasonToppicks";
 
-export default function Home() {
+const images = [
+  "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER_c80a47d2-f499-423c-b65f-4edc8127e43b.jpg?v=1778529168",
+  "https://wrogn.com/cdn/shop/files/WEBSITE-_BANNER-2BANNER-WROGN-ACTIVE.jpg?v=1777556912",
+  "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER-SS26.jpg?v=1781178696",
+  "https://wrogn.com/cdn/shop/files/WEBISTE-BANNER-2000X782_1.jpg?v=1782198223",
+  "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER-shirts_fe897867-25f1-4fae-9cc4-224fd000b2e9.jpg?v=1781179123"
+];
+
+function Home() {
   const [state, setState] = useState([]);
-const navigate = useNavigate()
+  const [current, setCurrent] = useState(0);
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-    axios.get("http://localhost:8015/data")
+    axios
+      .get("http://localhost:8015/data")
       .then((res) => setState(res.data))
       .catch((err) => console.log(err));
   }, []);
- const images = [
-    "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER_c80a47d2-f499-423c-b65f-4edc8127e43b.jpg?v=1778529168",
-    "https://wrogn.com/cdn/shop/files/WEBSITE-_BANNER-2BANNER-WROGN-ACTIVE.jpg?v=1777556912",
-    "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER-SS26.jpg?v=1781178696",
-    "https://wrogn.com/cdn/shop/files/WEBISTE-BANNER-2000X782_1.jpg?v=1782198223",
-    "https://wrogn.com/cdn/shop/files/WEBSITE-BANNER-shirts_fe897867-25f1-4fae-9cc4-224fd000b2e9.jpg?v=1781179123"
-  ];
-
-  const [current, setCurrent] = useState(0);
 
   const nextSlide = () => {
     setCurrent((prev) => (prev + 1) % images.length);
@@ -29,42 +32,45 @@ const navigate = useNavigate()
     setCurrent((prev) =>
       prev === 0 ? images.length - 1 : prev - 1
     );
-  }
-  useEffect(() => {
-  const interval = setInterval(() => {
-    setCurrent((prev) => (prev + 1) % images.length);
-  }, 3000);
+  };
 
-  return () => clearInterval(interval);
-}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <div className="container-fluid  position-relative">
+      <div className="container-fluid position-relative">
+        <img
+          src={images[current]}
+          alt="Banner"
+          className="w-100"
+          style={{
+            height: "500px",
+            objectFit: "cover"
+          }}
+        />
 
-      <img
-        src={images[current]}
-        alt="Banner"
-        className="w-100"
-        style={{ height: "500px", objectFit: "cover" }}
-      />
+        <button
+          type="button"
+          className="btn btn-dark position-absolute top-50 start-0 translate-middle-y ms-3"
+          onClick={prevSlide}
+        >
+          ❮
+        </button>
 
-      
-      <button
-        className="btn btn-dark position-absolute top-50 start-0 translate-middle-y ms-3"
-        onClick={prevSlide}
-      >
-        ❮
-      </button>
-
-      <button
-        className="btn btn-dark position-absolute top-50 end-0 translate-middle-y me-3"
-        onClick={nextSlide}
-      >
-        ❯
-      </button>
-    </div>
-      
-    
+        <button
+          type="button"
+          className="btn btn-dark position-absolute top-50 end-0 translate-middle-y me-3"
+          onClick={nextSlide}
+        >
+          ❯
+        </button>
+      </div>
 
       <div className="container mt-5">
         <h2 className="text-center fw-bold mb-5">
@@ -73,7 +79,10 @@ const navigate = useNavigate()
 
         <div className="row g-4">
           {state.map((el) => (
-            <div className="col-lg-3 col-md-4 col-sm-6" key={el.id}>
+            <div
+              className="col-lg-3 col-md-4 col-sm-6"
+              key={el.id}
+            >
               <Link
                 to={`/${el.category}`}
                 className="text-decoration-none text-dark"
@@ -82,7 +91,10 @@ const navigate = useNavigate()
                   <img
                     src={el.img}
                     className="card-img-top"
-                    style={{ height: "350px", objectFit: "cover" }}
+                    style={{
+                      height: "350px",
+                      objectFit: "cover"
+                    }}
                     alt={el.category}
                   />
 
@@ -91,137 +103,141 @@ const navigate = useNavigate()
                       {el.category}
                     </h5>
 
-                    <button className="btn btn-dark w-100 mt-2">
+                    <button
+                      type="button"
+                      className="btn btn-dark w-100 mt-2"
+                    >
                       SHOP NOW
                     </button>
                   </div>
                 </div>
               </Link>
             </div>
-            
           ))}
-            <video
-  className="w-100 mb-4"autoPlay
-  muted loop controls playsInline
-  style={{
-    height: "600px",
-    objectFit: "cover"
-  }}
->
-  <source
-    src="https://wrogn.com/cdn/shop/videos/c/vp/1aec96c21003479e8820ba118f08b7be/1aec96c21003479e8820ba118f08b7be.HD-720p-3.0Mbps-57504336.mp4?v=0"
-    type="video/mp4"
-  />
-</video>
+
+          <video
+            className="w-100 mb-4"
+            autoPlay
+            muted
+            loop
+            controls
+            playsInline
+            style={{
+              height: "600px",
+              objectFit: "cover"
+            }}
+          >
+            <source
+              src="https://wrogn.com/cdn/shop/videos/c/vp/1aec96c21003479e8820ba118f08b7be/1aec96c21003479e8820ba118f08b7be.HD-720p-3.0Mbps-57504336.mp4?v=0"
+              type="video/mp4"
+            />
+          </video>
         </div>
-      <div className="container-fluid mt-4">
-       <div><h3>TRENDING CATEGORIES</h3></div>
-  <div className="row g-1">
 
-    <div
-      className="col-md-6"
-      onClick={() => navigate("/tshirt")}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="position-relative overflow-hidden">
-        <img
-          src="https://wrogn.com/cdn/shop/files/T-SHIRTS_copy._e2be4c54-1cd1-421d-bdbe-60ffbd6deefd.jpg?v=1774011417&width=720"
-          alt="T-Shirt"
-          className="w-100 d-block"
-          style={{
-            height: "350px",
-            objectFit: "cover"
-          }}
-        />
+        <div className="container-fluid mt-4">
+          <div>
+            <h3>TRENDING CATEGORIES</h3>
+          </div>
 
+          <div className="row g-1">
+            <div
+              className="col-md-6"
+              onClick={() => navigate("/tshirt")}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="position-relative overflow-hidden">
+                <img
+                  src="https://wrogn.com/cdn/shop/files/T-SHIRTS_copy._e2be4c54-1cd1-421d-bdbe-60ffbd6deefd.jpg?v=1774011417&width=720"
+                  alt="T-Shirt"
+                  className="w-100 d-block"
+                  style={{
+                    height: "350px",
+                    objectFit: "cover"
+                  }}
+                />
+              </div>
+            </div>
+
+            <div
+              className="col-md-6"
+              onClick={() => navigate("/shirt")}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="position-relative overflow-hidden">
+                <img
+                  src="https://wrogn.com/cdn/shop/files/SHIRTS_copy..jpg?v=1774015803&width=720"
+                  alt="Shirt"
+                  className="w-100 d-block"
+                  style={{
+                    height: "350px",
+                    objectFit: "cover"
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row pt-1 px-1 g-1">
+            <div
+              className="col-md-4"
+              onClick={() => navigate("/shoes")}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="position-relative overflow-hidden">
+                <img
+                  src="https://wrogn.com/cdn/shop/files/FOOTWEAR_1_copy..jpg?v=1774016005&width=720"
+                  alt="Shoes"
+                  className="w-100 d-block"
+                  style={{
+                    height: "500px",
+                    objectFit: "cover"
+                  }}
+                />
+              </div>
+            </div>
+
+            <div
+              className="col-md-4"
+              onClick={() => navigate("/polo-shirt")}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="position-relative overflow-hidden">
+                <img
+                  src="https://wrogn.com/cdn/shop/files/POLO_00cc7cbf-b0f7-4d1b-ac2e-0f3c897cd728.jpg?v=1774015943&width=720"
+                  alt="Polos"
+                  className="w-100 d-block"
+                  style={{
+                    height: "500px",
+                    objectFit: "cover"
+                  }}
+                />
+              </div>
+            </div>
+
+            <div
+              className="col-md-4"
+              onClick={() => navigate("/pent")}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="position-relative overflow-hidden">
+                <img
+                  src="https://wrogn.com/cdn/shop/files/JEANS_copy..jpg?v=1774016034&width=720"
+                  alt="Pants"
+                  className="w-100 d-block"
+                  style={{
+                    height: "500px",
+                    objectFit: "cover"
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <SeasonTopPicks />
       </div>
-    </div>
-
-    <div
-      className="col-md-6"
-      onClick={() => navigate("/shirt")}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="position-relative overflow-hidden">
-        <img
-          src="https://wrogn.com/cdn/shop/files/SHIRTS_copy..jpg?v=1774015803&width=720"
-          alt="Shirt"
-          className="w-100 d-block"
-          style={{
-            height: "350px",
-            objectFit: "cover"
-          }}
-        />
-      </div>
-    </div>
-
-  </div>
-
-  <div className="row pt-1 px-1 g-1">
-  
-    <div
-      className="col-md-4"
-      onClick={() => navigate("/shoes")}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="position-relative overflow-hidden">
-        <img
-          src="https://wrogn.com/cdn/shop/files/FOOTWEAR_1_copy..jpg?v=1774016005&width=720"
-          alt="Shoes"
-          className="w-100 d-block"
-          style={{
-            height: "500px",
-            objectFit: "cover"
-          }}
-        />
-
-      </div>
-    </div>
-
-    <div
-      className="col-md-4"
-      onClick={() => navigate("/polo-shirt")}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="position-relative   overflow-hidden">
-        <img
-          src="https://wrogn.com/cdn/shop/files/POLO_00cc7cbf-b0f7-4d1b-ac2e-0f3c897cd728.jpg?v=1774015943&width=720"
-          alt="Polos"
-          className="w-100 d-block"
-          style={{
-            height: "500px",
-            objectFit: "cover"
-          }}
-        />
-      </div>
-    </div>
-
-    <div
-      className="col-md-4"
-      onClick={() => navigate("/pent")}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="position-relative  overflow-hidden">
-        <img
-          src="https://wrogn.com/cdn/shop/files/JEANS_copy..jpg?v=1774016034&width=720"
-          alt="Pants"
-          className="w-100 d-block"
-          style={{
-            height: "500px",
-            objectFit: "cover"
-          }}
-        />
-      </div>
-    </div>
-
-  </div>
-
-</div>
-<SeasonTopPicks/>
-      </div>
-
-  
-    
     </>
   );
 }
+
+export default Home;
--- src/Comp/Nav.jsx
+++ src/Comp/Nav.jsx
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-function Nav() {
+const categories = [
+  "shoes",
+  "watch",
+  "shirt",
+  "perfume",
+  "tshirt",
+  "polo-shirt",
+  "jackets",
+  "hoodies",
+  "footwear",
+  "slider",
+  "pent",
+  "cargo-pent",
+  "backpack"
+];
 
+function Nav() {
   const [searchOpen, setSearchOpen] = useState(false);
   const [search, setSearch] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
   const navigate = useNavigate();
 
-  const categories = [
-    "shoes",
-    "watch",
-    "shirt",
-    "perfume",
-    "tshirt",
-    "polo-shirt",
-    "jackets",
-    "hoodies",
-    "footwear",
-    "slider",
-    "pent",
-    "cargo-pent",
-    "backpack"
-  ];
-
   useEffect(() => {
-
     const text = search.toLowerCase().trim();
 
     if (text === "") {
@@ -39,93 +37,63 @@ function Nav() {
     );
 
     setSuggestions(result);
-
   }, [search]);
 
-
   const handleSearch = (e) => {
-
     e.preventDefault();
 
     const value = search.trim();
 
     if (value !== "") {
-
-      navigate(
-        `/search?search=${encodeURIComponent(value)}`
-      );
-
+      navigate(`/search?search=${encodeURIComponent(value)}`);
       setSearch("");
       setSuggestions([]);
       setSearchOpen(false);
-
     }
-
   };
 
-
   const openCategory = (category) => {
-
     navigate(`/${category}`);
-
     setSearch("");
     setSuggestions([]);
     setSearchOpen(false);
-
   };
 
-
   const toggleSearch = () => {
-
     setSearchOpen(!searchOpen);
     setSearch("");
     setSuggestions([]);
-
   };
-
 
   return (
     <>
-
-      <div className="bg-dark text-white py-2">
-
-        <marquee>
+      <div className="offer-bar bg-dark text-white">
+        <div className="offer-text">
           Upto | 50% Off&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           Upto | 50% Off&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           Upto | 50% Off&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           Upto | 50% Off
-        </marquee>
-
+        </div>
       </div>
 
-
       <nav className="navbar navbar-expand-lg bg-white shadow-sm">
-
         <div className="container-fluid px-3 px-lg-4">
-
-          <Link
-            to="/"
-            className="navbar-brand"
-          >
-
+          <Link to="/" className="navbar-brand">
             <img
               src="https://wrogn.com/cdn/shop/files/logo_icon_1_bd4a99ba-1c20-43de-81ff-1f5fb0685b8e.svg?v=1736489168&width=50"
-              alt="Logo"
+              alt="Wrogn Logo"
               width="50"
               height="50"
             />
-
           </Link>
 
-
           <div className="d-flex align-items-center gap-1 d-lg-none">
-
             <button
               type="button"
               className="btn border-0 p-2"
               onClick={toggleSearch}
+              aria-label="Search"
             >
-
               <i
                 className={
                   searchOpen
@@ -133,19 +101,15 @@ function Nav() {
                     : "bi bi-search fs-5"
                 }
               ></i>
-
             </button>
-
 
             <Link
               to="/cart"
               className="btn border-0 p-2"
+              aria-label="Cart"
             >
-
               <i className="bi bi-cart3 fs-5"></i>
-
             </Link>
-
 
             <button
               className="btn border-0 p-2"
@@ -154,8 +118,8 @@ function Nav() {
               data-bs-target="#mainNavbar"
               aria-controls="mainNavbar"
               aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-
               <span
                 style={{
                   fontSize: "28px",
@@ -165,33 +129,25 @@ function Nav() {
               >
                 ☰
               </span>
-
             </button>
-
           </div>
-
 
           <div
             className="collapse navbar-collapse"
             id="mainNavbar"
           >
-
             {!searchOpen && (
-
               <ul className="navbar-nav mx-auto">
-
                 <li className="nav-item dropdown">
-
-                  <a
-                    className="nav-link dropdown-toggle text-dark fw-semibold"
-                    href="#"
+                  <button
+                    type="button"
+                    className="nav-link dropdown-toggle text-dark fw-semibold border-0 bg-transparent"
                     data-bs-toggle="dropdown"
                   >
                     BESTSELLER
-                  </a>
+                  </button>
 
                   <ul className="dropdown-menu">
-
                     <li>
                       <Link
                         className="dropdown-item"
@@ -218,24 +174,19 @@ function Nav() {
                         JACKETS
                       </Link>
                     </li>
-
                   </ul>
-
                 </li>
 
-
                 <li className="nav-item dropdown">
-
-                  <a
-                    className="nav-link dropdown-toggle text-dark fw-semibold"
-                    href="#"
+                  <button
+                    type="button"
+                    className="nav-link dropdown-toggle text-dark fw-semibold border-0 bg-transparent"
                     data-bs-toggle="dropdown"
                   >
                     WINTERWEAR
-                  </a>
+                  </button>
 
                   <ul className="dropdown-menu">
-
                     <li>
                       <Link
                         className="dropdown-item"
@@ -253,24 +204,19 @@ function Nav() {
                         JACKETS
                       </Link>
                     </li>
-
                   </ul>
-
                 </li>
 
-
                 <li className="nav-item dropdown">
-
-                  <a
-                    className="nav-link text-dark dropdown-toggle fw-semibold"
-                    href="#"
+                  <button
+                    type="button"
+                    className="nav-link text-dark dropdown-toggle fw-semibold border-0 bg-transparent"
                     data-bs-toggle="dropdown"
                   >
                     ALL PRODUCTS
-                  </a>
+                  </button>
 
                   <ul className="dropdown-menu">
-
                     <li>
                       <Link
                         className="dropdown-item"
@@ -315,24 +261,19 @@ function Nav() {
                         PERFUME
                       </Link>
                     </li>
-
                   </ul>
-
                 </li>
 
-
                 <li className="nav-item dropdown">
-
-                  <a
-                    className="nav-link text-dark dropdown-toggle fw-semibold"
-                    href="#"
+                  <button
+                    type="button"
+                    className="nav-link text-dark dropdown-toggle fw-semibold border-0 bg-transparent"
                     data-bs-toggle="dropdown"
                   >
                     FOOTWEAR
-                  </a>
+                  </button>
 
                   <ul className="dropdown-menu">
-
                     <li>
                       <Link
                         className="dropdown-item"
@@ -359,24 +300,19 @@ function Nav() {
                         SLIDERS
                       </Link>
                     </li>
-
                   </ul>
-
                 </li>
 
-
                 <li className="nav-item dropdown">
-
-                  <a
-                    className="nav-link text-dark dropdown-toggle fw-semibold"
-                    href="#"
+                  <button
+                    type="button"
+                    className="nav-link text-dark dropdown-toggle fw-semibold border-0 bg-transparent"
                     data-bs-toggle="dropdown"
                   >
                     BOTTOMWEAR
-                  </a>
+                  </button>
 
                   <ul className="dropdown-menu">
-
                     <li>
                       <Link
                         className="dropdown-item"
@@ -394,57 +330,41 @@ function Nav() {
                         CARGO PANTS
                       </Link>
                     </li>
-
                   </ul>
-
                 </li>
 
-
                 <li className="nav-item">
-
                   <Link
                     className="nav-link text-dark fw-semibold"
                     to="/backpack"
                   >
                     BACKPACKS
                   </Link>
-
                 </li>
 
-
                 <li className="nav-item">
-
                   <Link
                     className="nav-link text-dark fw-semibold"
                     to="/polo-shirt"
                   >
                     POLOS
                   </Link>
-
                 </li>
-
               </ul>
-
             )}
 
-
             {searchOpen && (
-
               <div className="position-relative flex-grow-1 mx-lg-4 my-3">
-
                 <form
                   onSubmit={handleSearch}
                   className="d-flex"
                 >
-
                   <input
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Search category..."
                     value={search}
-                    onChange={(e) =>
-                      setSearch(e.target.value)
-                    }
+                    onChange={(e) => setSearch(e.target.value)}
                     autoFocus
                   />
 
@@ -452,16 +372,11 @@ function Nav() {
                     type="submit"
                     className="btn btn-dark ms-2 px-4"
                   >
-
                     <i className="bi bi-search"></i>
-
                   </button>
-
                 </form>
 
-
                 {suggestions.length > 0 && (
-
                   <div
                     className="position-absolute bg-white shadow border rounded w-100 mt-1"
                     style={{
@@ -470,43 +385,30 @@ function Nav() {
                       overflowY: "auto"
                     }}
                   >
-
                     {suggestions.map((category) => (
-
                       <div
                         key={category}
                         className="px-3 py-3 border-bottom fw-semibold"
                         style={{
                           cursor: "pointer"
                         }}
-                        onClick={() =>
-                          openCategory(category)
-                        }
+                        onClick={() => openCategory(category)}
                       >
-
                         {category.toUpperCase()}
-
                       </div>
-
                     ))}
-
                   </div>
-
                 )}
-
               </div>
-
             )}
 
-
             <div className="d-none d-lg-flex align-items-center gap-2 ms-lg-3">
-
               <button
                 type="button"
                 className="btn border-0"
                 onClick={toggleSearch}
+                aria-label="Search"
               >
-
                 <i
                   className={
                     searchOpen
@@ -514,31 +416,19 @@ function Nav() {
                       : "bi bi-search"
                   }
                 ></i>
-
               </button>
-
 
               <Link
                 to="/cart"
                 className="btn btn-dark d-flex align-items-center gap-2 px-3"
               >
-
                 <i className="bi bi-cart3"></i>
-
-                <span>
-                  Cart
-                </span>
-
+                <span>Cart</span>
               </Link>
-
             </div>
-
           </div>
-
         </div>
-
       </nav>
-
     </>
   );
 }
